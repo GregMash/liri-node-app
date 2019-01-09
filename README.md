4a2247cecd091cebcb79a0ad48b81ecd5d7f1fed
Link <username@users.noreply.github.com>
2019-01-09 03:34:29 -0700

    polish readme, commented liri.js, polish

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # liri-node-app
+
+This project is a node line Language Interpretation and Recognition Interface.
+It will take in a command and parameters and return to the user results to both your bash/terminal and to the log.txt file.
+Currently it can take 3 different commdands: spotify-this, movie-this, and do-what-it-says
+Spotify will find you 5 different hits for songs you type in
+OMDB will return movie information to you when you search for a movie title
+do-what-it-says will take the text from each line of the random.txt file and perform an appropriate action based on the command entered for each line.
+*If you simply enter commands without parameters, the program will default to certain songs and movies.
+*If you enter an invalid command the program will let you know correct ways of entering valid commands.
+
+
+To use the program:
+-Clone and Navigate your bash/terminal to the directory of the program
+-You will need to install packages using npm i followed by each package
+-You will need to install axios, node-spotify-API, Moment, DotENV, and fs
+-You will also need to obtain your own spotify api keys and your own omdb api key
+-Put those keys in a .env file using the following syntax
+    -# Spotify and OMDB API keys
+    -SPOTIFY_ID=Your_Spotify_ID
+    -SPOTIFY_SECRET=Your_Spotify_Secret
+    -OMDB_KEY=Your_OMDB_Key
+
+Then you are free to begin using the program.
+*Make sure you always begin your search with: node liri.js command parameters*
+    example: node liri.js spotify-this free falling
--- liri.js
+++ liri.js
@@ -1,69 +1,111 @@
 //========================== Global Variables =============================================
+
+//require various packages for application to run
 const Dotenv = require("dotenv").config();
 const Keys = require('./keys');
 const Spotify = require('node-spotify-api');
 const Moment = require('moment');
 const axios = require('axios');
+const fs = require('fs');
 //format for using the current time displayed for each call
 let now = Moment().format('LLLL');
 //this will grab the command and its parameters from the user input
-const [node, file, command, ...parameters] = process.argv;
+let [node, file, command, ...parameters] = process.argv;
+// this will be used to write the result to the logged file every time a command is executed
+let output;
 
+//=============================== Functions =============================================
 
-//========================== Functions =============================================
+//this function will use parameters to search spotify for information on song and return it to bash/terminal line and log.txt file
 function spotifyThis() {
   const spotify = new Spotify(Keys.spotify);
-  spotify.search({ type: 'track', query: parameters, limit: 7 }, function (err, data) {
+  if (parameters === []) {
+    parameters = "The Sign Ace of Base";
+  };
+  spotify.search({ type: 'track', query: parameters, limit: 5 }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     };
     const songs = data.tracks.items;
     console.log(`
-    
     ---------------Your Spotify search on ${now}------------`);
     for (let i = 0; i < songs.length; i++) {
-      console.log(`
+      output = (`
 Song Name: ${songs[i].name}
 Artist: ${songs[i].artists[0].name}
 Album: ${songs[i].album.name}
 Preview Link: ${songs[i].preview_url}
-      `);
+            `);
+      console.log(output);
     };
-    console.log('     ----------------------------------------------------------------------------------');
+    console.log(`     
+    ----------------------------------------------------------------------------------`);
+    fs.appendFile('log.txt', output, err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Added result to file: log.txt");
+      }
+    });
   });
 };
 
-
+//this function will use axios call and get data back from omdb and return it to bash/terminal line and log.txt file
 function getMovie() {
+  if (parameters === []) {
+    parameters = "Mr. Nobody";
+  };
   axios.get(`http://www.omdbapi.com/?t=${parameters}&apikey=${Keys.omdb.id}`)
     .then(
       function (response) {
-        //console.log(response.data);
-        const {Title, Year, imdbRating, Country, Language, Plot, Actors}  = response.data
+        const { Title, Year, imdbRating, Ratings, Country, Language, Plot, Actors } = response.data;
         console.log(`
-    
       ---------------Your movie search on ${now}------------`);
-        console.log(`
+        output = (`
+-----------------------------------------------------------------------------------
 Movie Title: ${Title}
 Year: ${Year}
 Country: ${Country}
 Language: ${Language}
 Actors: ${Actors}
 IMDB Rating: ${imdbRating}
-Rotten Tomatoes Rating: ${imdbRating}
+Rotten Tomatoes Rating: ${Ratings[2].Value}
 Plot: ${Plot}
-      ----------------------------------------------------------------------------------`);
-      }
-    );
+----------------------------------------------------------------------------------`);
+        console.log(output);
+        fs.appendFile('log.txt', output, err => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Added result to file: log.txt");
+          }
+        });
+      });
 };
 
-
+//this function will run all commands in the random.txt file
 function doWhatItSays() {
-
+  fs.readFile('random.txt', 'utf-8', function (err, data) {
+    if (err) {
+      console.log(err)
+    };
+    const dataArray = data.split('\n');
+    for (let i = 0; i < dataArray.length; i++) {
+      let separateData = dataArray[i].split(',');
+      command = separateData[0];
+      parameters = separateData[1];
+      if (command === "spotify-this") {
+        spotifyThis();
+      } else if (command === "movie-this") {
+        getMovie();
+      };
+    };
+  });
 };
 
-//========================== Main Process =============================================
+//============================== Main Process =============================================
 
+//the switch case for when the user enters both valid and invalid commands 
 switch (command) {
   case 'spotify-this':
     spotifyThis();
